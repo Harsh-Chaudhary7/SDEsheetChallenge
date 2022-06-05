12c31d1867298c26e2747ccf43f00aae29e79523
Harsh Chauhdary <70788113+Harsh-Chaudhary7@users.noreply.github.com>
2022-06-05 15:52:01 +0530

    Add files via upload

---
+++ Day1/Search2Dmat.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Template
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define Harsh_Chaudhary std::ios::sync_with_stdio(false);
+#define H_C std::cin.tie(nullptr);
+
+typedef long long int               ll;
+typedef vector<int>                 vi;
+typedef vector<ll>                  vl;
+typedef vector<vector<int>>         vvi;
+typedef pair<int, int>              pi;
+typedef vector<pair<int, int>>      vpi;
+typedef vector<pair<ll, ll>>        vpl;
+typedef unordered_map<int, int>     umi;
+typedef unordered_map<char, int>    ums;
+typedef map<int, char>              ms;
+typedef map<int, int>               mi;
+
+
+#define inp(a, n)                   for(int Ele=0; Ele<(n); Ele++)std::cin>>a[Ele]
+#define inpv(a, n)                  for(int Ele=0; Ele<(n); Ele++){int val;cin>>val;a.push_back(val);}
+#define inps(a, n, sum)             for(int Ele=0; Ele<(n); Ele++){std::cin>>a[Ele]; sum += a[Ele];}
+#define out(a, n)                   for(int Ele=0; Ele<(n); Ele++)std::cout<<a[Ele]<<endl
+#define inp1(a, n)                  for(int Ele=1; Ele<=(n); Ele++)cin>>a[Ele]
+#define max(a, b)                   (a < b ? b : a)
+#define min3(a, b, c)               ((a<b)?(a<c)? a :c :(b<c)? b: c)
+#define min(a, b)                   ((a > b) ? b : a)
+
+
+#define INF                         1e18
+#define lb                          lower_bound
+#define ub                          upper_bound
+#define min_element                 *min_element
+#define max_element                 *max_element
+#define pb                          push_back
+#define ll                          long long 
+#define rr return
+
+#define mod                       (in1e9+7
+#define mem(x,i) memset((x),(i),sizeof((x)))
+
+// Template
+
+void solve(vector<vector<int>> mat, int m, int n,int target){
+    for(int i=0; i<m; i++){
+        if(target==mat[i][n-1] || target==mat[i][0]){
+            cout<<true; return;
+        }
+        if(target<mat[i][n-1]){
+            int l = 0, r = n-1, mid;
+            while(l<=r){
+                int mid = (l+r)/2;
+                if(mat[i][mid]==target){
+                    cout<<true; return;
+                }
+                else if(mat[i][mid]>target)
+                    l=mid+1;
+                else
+                    r=mid-1;
+            }
+        }
+    }
+    cout<<false; 
+}
+
+
+
+int main() {    
+    fast_io
+    Harsh_Chaudhary
+    H_C
+
+    int t;
+    cin>>t;
+    while(t--){
+    int n,m,target;
+    cin>>n>>m>>target;
+    
+    vector<vector<int>> arr(n,vector<int>(m));
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            cin>>arr[i][j];
+    
+    solve(arr,n,m,target);
+    
+    cout<<endl;
+}
+    
+
+    return 0;
+}
+
+
+
